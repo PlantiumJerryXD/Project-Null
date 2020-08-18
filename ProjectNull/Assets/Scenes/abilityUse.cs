e7b6c9fcbe959ab0c13c4548ae288f6fe1058472
johnjohnjohnk <69476195+johnjohnjohnk@users.noreply.github.com>
2020-08-18 13:15:52 -0400

    UI-ability icons + start of usage script

---
+++ ProjectNull/Assets/Scenes/abilityUse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityUse : MonoBehaviour
+{
+    public GameObject A;
+    public GameObject X;
+    public GameObject B;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+    }
+    
+    
+}
